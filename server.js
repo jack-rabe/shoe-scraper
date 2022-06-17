4e199a72f041a909a2601d7c8ef70ada8b416db2
= <jhrabe@wisc.edu>
2022-06-17 18:24:46 -0400

    add support for one brand to be selected in form

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+
 const path = require('path')
 const data = require('./data/merged_shoes.json')
 
@@ -10,18 +11,26 @@ app.use(express.static(path.resolve(__dirname, 'node_modules/bootswatch/dist/cyb
 app.use(express.static(path.resolve(__dirname, 'images')));
 app.use(express.static(path.resolve(__dirname, 'js')));
 
+// parse request bodies
+app.use(express.json());
+
 app.get('/', (_req, res) => {
-	res.render('pages/index', {'data': data});
+	res.render('pages/index', {
+		'data': data,
+	});
 })
 
-app.get('/data', (_req, res) => {
+app.get('/data', (req, res) => {
 	shoes = data['shoes'];
+	brand = req.query.brand;
+
 	shoes = shoes.filter( shoe => {
-		return shoe.brand == 'nike'
+		return shoe.brand == brand; 
 	});
 	result = {
 		'shoes': shoes,
-		'brands': ['nike'],
+		'brands': data.brands,
+		'current_brand': brand,
 		'count': shoes.length 
 	}
 
