cf9560f847e533c7dff506e6b8ebe62f9d50af15
= <jhrabe@wisc.edu>
2022-09-14 20:12:28 -0500

    bind to correct port for Heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const sortShoes = require('./middleware/sortShoes')
 const renderPage = require('./middleware/renderPage')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'pug')
 app.use(
